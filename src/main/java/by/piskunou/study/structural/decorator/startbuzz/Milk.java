41e0844a1495029e7ec8f1dd4bc9789197f91907
cichan <6684177pt@gmail.com>
2023-12-24 13:54:59 +0300

    add Milk decorator

---
+++ src/main/java/by/piskunou/study/structural/decorator/startbuzz/Milk.java
@@ -0,0 +1,15 @@
+package by.piskunou.study.structural.decorator.startbuzz;
+
+public class Milk extends CondimentDecorator {
+	public Milk(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	public String getDescription() {
+		return beverage.getDescription() + ", Milk";
+	}
+
+	public double cost() {
+		return .10 + beverage.cost();
+	}
+}
