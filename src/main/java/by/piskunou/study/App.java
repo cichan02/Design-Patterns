18a114407beb20312d2158cb82f449dbcba9b165
Cichan Piskunou <t.piskunov@besmart.by>
2024-02-12 13:29:48 +0300

    init #command

--- src/main/java/by/piskunou/study/App.java
+++ src/main/java/by/piskunou/study/App.java
@@ -1,10 +1,10 @@
 package by.piskunou.study;
 
-import by.piskunou.study.experimental.factory.abstractf.MyAbstractFactoryMain;
+import by.piskunou.study.behavioral.command.CommandMain;
 
 public class App {
     public static void main(String[] args) {
-        Main main = new MyAbstractFactoryMain();
+        Main main = new CommandMain();
         main.start();
     }
 }
---
+++ src/main/java/by/piskunou/study/behavioral/command/CommandMain.java
@@ -0,0 +1,10 @@
+package by.piskunou.study.behavioral.command;
+
+import by.piskunou.study.Main;
+
+public class CommandMain implements Main {
+    @Override
+    public void start() {
+        System.out.println("Hello World!");
+    }
+}
